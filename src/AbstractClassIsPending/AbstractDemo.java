b4610c84fb6d84a7a13e418aeada009b0fb51e2e
binulalpb <binulalpb@gmail.com>
2019-01-18 17:47:48 -0500

    ABSTRACT

---
+++ src/AbstractClassIsPending/AbstractDemo.java
@@ -0,0 +1,14 @@
+package AbstractClassIsPending;
+
+public class AbstractDemo {
+
+
+
+
+    public static void main(String[] args) {
+        int i =8;
+
+        String hello  ="the world is yours , swag  se swagath  ";
+        System.out.println(hello);
+    }
+}
